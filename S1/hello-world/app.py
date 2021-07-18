b95adec1e3b0b1f7559920e56d9197c2b2545403
Amin Taheri <amintaheri23@eyahoo.com>
2021-07-18 12:01:41 +0430

    commenting for how to run

--- S1/hello-world/app.py
+++ S1/hello-world/app.py
@@ -1,3 +1,6 @@
+#  This is a hello world example for flask. 
+#  IF the name of the file wan not app.py, you should do `export FLASK_APP=hello` 
+# To run this, you sould run `flask run` in the directory that app.py exists. 
 from flask import Flask, render_template
 
 app = Flask(__name__)
