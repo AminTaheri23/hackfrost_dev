f15d7b0d0672ae32141571f416c5a9aa63d6980f
Amin Taheri <amintaheri23@eyahoo.com>
2021-07-18 12:23:33 +0430

    added escape hello world and working in progress tempate hello world

--- S1/hello-world/app.py
+++ S1/hello-world/app.py
@@ -1,13 +1,24 @@
-#  This is a hello world example for flask. 
-#  IF the name of the file wan not app.py, you should do `export FLASK_APP=hello` 
+# This is a hello world example for flask. 
+# IF the name of the file wan not app.py, you should do `export FLASK_APP=hello` 
 # To run this, you sould run `flask run` in the directory that app.py exists. 
+# To activate the debugger: 
+# export FLASK_ENV=development
+
 from flask import Flask, render_template
+from flask.templating import render_template_string
+from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route("/<name>")
+def hello_world_escape(name):
+    return f"Hello, {escape(name)}" # escaping will prevent script injectino attacks!
+    
 @app.route("/")
 def hello_world():
-    return render_template("hello.html") #hello world
+    return "Hello World!" # Hello world
 
-#To activate the debugger: 
-# export FLASK_ENV=development
+@app.route("/template")
+def hello_world_template():
+    return render_template("template/hello.html") # Hello world with template
+    
