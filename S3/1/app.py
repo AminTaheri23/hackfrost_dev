3bb1e6bb30691ec4623999bc2f4daf6291f72455
Amin Taheri <amintaheri23@eyahoo.com>
2021-07-24 20:12:08 +0430

    added blog page in app.py and in the nav bar and also the template dir

--- S3/1/app.py
+++ S3/1/app.py
@@ -40,5 +40,11 @@ def projects_page():
 def contacts_page():
     return render_template("contact.html", api=CONTACT_FORM_ACTION)
 
+@app.route("/blog")
+def blog_page():
+    return render_template("blog.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
